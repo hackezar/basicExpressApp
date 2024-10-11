8bf77102c59be5d562265a22585ad169049205c8
hackezar <107069171+hackezar@users.noreply.github.com>
2024-10-11 15:33:55 -0400

    1

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`My first Express app - listening oon port ${PORT}!`));
